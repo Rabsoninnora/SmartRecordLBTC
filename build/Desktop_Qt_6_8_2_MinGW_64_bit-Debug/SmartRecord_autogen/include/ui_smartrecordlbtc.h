bc687b2b0ed028ec4ee3ac59f0f231cdf207ded1
Rabson Innocent Phiri <rabsoninnora@gmail.com>
2025-03-28 15:14:51 +0200

    modified ui

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/SmartRecord_autogen/include/ui_smartrecordlbtc.h
+++ build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/SmartRecord_autogen/include/ui_smartrecordlbtc.h
@@ -13,6 +13,7 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -22,6 +23,8 @@ class Ui_SmartRecordLBTC
 {
 public:
     QWidget *centralwidget;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -29,9 +32,21 @@ public:
     {
         if (SmartRecordLBTC->objectName().isEmpty())
             SmartRecordLBTC->setObjectName("SmartRecordLBTC");
-        SmartRecordLBTC->resize(800, 600);
+        SmartRecordLBTC->resize(800, 412);
         centralwidget = new QWidget(SmartRecordLBTC);
         centralwidget->setObjectName("centralwidget");
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(0, -10, 201, 401));
+        QSizePolicy sizePolicy(QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(pushButton->sizePolicy().hasHeightForWidth());
+        pushButton->setSizePolicy(sizePolicy);
+        pushButton->setMaximumSize(QSize(16777215, 16777215));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName("pushButton_2");
+        pushButton_2->setGeometry(QRect(200, 0, 601, 61));
         SmartRecordLBTC->setCentralWidget(centralwidget);
         menubar = new QMenuBar(SmartRecordLBTC);
         menubar->setObjectName("menubar");
@@ -49,6 +64,8 @@ public:
     void retranslateUi(QMainWindow *SmartRecordLBTC)
     {
         SmartRecordLBTC->setWindowTitle(QCoreApplication::translate("SmartRecordLBTC", "SmartRecordLBTC", nullptr));
+        pushButton->setText(QCoreApplication::translate("SmartRecordLBTC", "PushButton", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("SmartRecordLBTC", "PushButton", nullptr));
     } // retranslateUi
 
 };
